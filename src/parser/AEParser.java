09aae8676c915e6694f0114afe0449c7b4774b29
jevo1900 <jevo1900@gmail.com>
2019-12-09 15:14:58 +0100

    Update AEParser.java

--- src/parser/AEParser.java
+++ src/parser/AEParser.java
@@ -15,13 +15,15 @@ import java.io.InputStreamReader;
  * @author Usuario
  */
 public class AEParser {
-    
+
     private AEContentReader aeContentReader;
 
     public AEParser(String file, AEContentReader aeContentReader) {
-        if(aeContentReader==null) throw new NullPointerException("An AEContentReader Object is Required in order to process the ae file");
-        this.aeContentReader=aeContentReader;
-        AEList list=new AEList();
+        if (aeContentReader == null) {
+            throw new NullPointerException("An AEContentReader Object is Required in order to process the ae file");
+        }
+        this.aeContentReader = aeContentReader;
+        AEList list = new AEList();
         try {
             BufferedReader br;
             br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
@@ -32,36 +34,44 @@ public class AEParser {
                 } catch (IOException ex) {
                 }
                 AEObject check;
-                
+
                 check = Checker.CheckAndType(readLine);
-                
+
                 list.add(check);
             }
         } catch (IOException ex) {
         }
-        
-        
+
         int num = 0;
         for (AEObject string : list) {
             int n = num;
-            if (string == null) {
+            if (string.getKey() == null) {
                 this.aeContentReader.decrementLevel(--num);
+                if (string.hasAttrs()) {
+                    this.aeContentReader.getAttrs(string.getAttrs());
+                }
             } else if (string.getValue() == null) {
-                this.aeContentReader.getContent(num,string.getKey(),null);
+                this.aeContentReader.getContent(num, string.getKey(), null);
                 this.aeContentReader.hasChildElements();
-                if(string.hasAttrs()) this.aeContentReader.getAttrs(string.getAttrs());
+                if (string.hasAttrs()) {
+                    this.aeContentReader.getAttrs(string.getAttrs());
+                }
                 if (num == 0) {
                     //evento elemento raiz
                 }
                 this.aeContentReader.incrementLevel(++num);
             } else if (string.getValue() != null) {
-                this.aeContentReader.getContent(num,string.getKey(),string.getValue());
-                if(string.hasAttrs()) this.aeContentReader.getAttrs(string.getAttrs());
+                this.aeContentReader.getContent(num, string.getKey(), string.getValue());
+                if (string.hasAttrs()) {
+                    this.aeContentReader.getAttrs(string.getAttrs());
+                }
+            } else {
+
             }
             if (n != num) {
                 //incremento o decremento
             }
         }
     }
-    
+
 }
